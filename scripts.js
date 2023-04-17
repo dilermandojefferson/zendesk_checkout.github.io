880ff13ebead2ceefbae4f67929660bc046ebb5a
Dilermando J Silva de souza <105756743+dilermandojefferson@users.noreply.github.com>
2023-04-17 13:13:45 -0300

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -2,3 +2,17 @@ let frameElement;
 let doc;
 
 zE("webWidget", "open");
+
+zE("webWidget:on", "userEvent", function (event) {
+  if (event.action == "Web Widget Opened") {
+    setTimeout(() => hideHeader(), 3000);
+  }
+
+  if (event.action == "Contact Form Shown") {
+    changeInputs();
+  }
+
+  if (event.action == "Contact Form Submitted") {
+    window.ReactNativeWebView.postMessage(JSON.stringify({ sended: true }));
+  }
+});
