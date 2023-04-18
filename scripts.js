42a0f80a1b4d43c55452a42ac29e789356c211f6
Dilermando J Silva de souza <105756743+dilermandojefferson@users.noreply.github.com>
2023-04-18 12:45:11 -0300

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -3,17 +3,19 @@ let doc;
 
 zE('webWidget', 'open');
 
-zE("webWidget:on", "userEvent", function (event) {
-  if (event.action == "Web Widget Opened") {
-  WebViewChannel.postMessage('WidgetOpened')}
- 
-  if (event.action == "Contact Form Shown") {
-  WebViewChannel.postMessage('FormShown')}
- 
-  if (event.action == "Contact Form Submitted") {
-  WebViewChannel.postMessage('FormSubmitted')}
-  
-});
 zE('webWidget:on', 'close', function() {
 WebViewChannel.postMessage('closeWebView');
 });
+
+zE("webWidget:on", "userEvent", function (event) {
+  if (event.action == "Web Widget Opened") {
+  WebViewChannel.postMessage('WidgetOpened');}
+ 
+  if (event.action == "Contact Form Shown") {
+  WebViewChannel.postMessage('FormShown');}
+ 
+  if (event.action == "Contact Form Submitted") {
+  WebViewChannel.postMessage('FormSubmitted');}
+  
+});
+
