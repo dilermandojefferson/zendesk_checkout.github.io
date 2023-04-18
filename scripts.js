1d0416488d26ee11f7a67b576e4faa8f0d7f9548
Dilermando J Silva de souza <105756743+dilermandojefferson@users.noreply.github.com>
2023-04-18 18:02:42 -0300

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,9 +1,9 @@
+zE("messenger:on", "open", function () {
+  WebViewChannel.postMessage('openWebView')
+});
+
 zE('messenger', 'open');
 
-
-zE("messenger:on", "open", function () {
-  console.log(`You have opened the messaging Web Widget`)
-});
 zE("messenger:on", "close", function () {
   WebViewChannel.postMessage('closeWebView')
 });
