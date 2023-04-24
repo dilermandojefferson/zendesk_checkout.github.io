60395853cd680bed94c32c54f85d6d28162a4b80
Dilermando J Silva de souza <105756743+dilermandojefferson@users.noreply.github.com>
2023-04-23 23:16:40 -0300

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -3,9 +3,9 @@ zE("webWidget", "open");
 
 //webWidget
 zE("webWidget:on", "userEvent", function (event) {
-  if (event.action == "Web Widget Opened	") {
+  if (event.action == "Web Widget Opened") {
     WebViewChannel.postMessage('openWebView');
-  }
+  });
   zE("webWidget:on", "Web Widget Minimised", function () {
     WebViewChannel.postMessage('closeWebView');
   });
@@ -19,9 +19,11 @@ zE("webWidget:on", "userEvent", function (event) {
 zE("messenger:on", "userEvent", function (event) {
   if (event.action == "open") {
     WebViewChannel.postMessage('openWebView');
-  }
+  });
   if (event.action == "close") {
     WebViewChannel.postMessage('closeWebView');
-  }
+  });
 });
+
+zE("messenger:set", "cookies", true);
 
