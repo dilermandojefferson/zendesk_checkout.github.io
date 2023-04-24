3d28b34c3a4ed271c37a989d2bf1e7623f14b1e2
Dilermando J Silva de souza <105756743+dilermandojefferson@users.noreply.github.com>
2023-04-23 22:31:08 -0300

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,9 +1,16 @@
 zE("messenger:on", "open", function () {
   WebViewChannel.postMessage('openWebView')
 });
+zE("webWidget:on", "open", function () {
+  WebViewChannel.postMessage('openWebView')
+});
 
 zE('messenger', 'open');
+zE("webWidget", "open");
 
 zE("messenger:on", "close", function () {
+  WebViewChannel.postMessage('closeWebView')
+});
+zE("webWidget:on", "close", function () {
   WebViewChannel.postMessage('closeWebView')
 });
