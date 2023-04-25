756f1fcd6c9cf1293b2cf34e7400f319763574bc
Dilermando J Silva de souza <105756743+dilermandojefferson@users.noreply.github.com>
2023-04-25 15:31:23 -0300

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,11 +1,13 @@
 zE('webWidget', 'open');
-// zE('messenger', 'open');
 
 zE('webWidget:on', 'close', function() {
 WebViewChannel.postMessage('closeWebView');
 });
 
 zE("webWidget:on", "userEvent", function (event) {
+  if (event.action == "Web Widget Opened") {
+    setTimeout(() => hideHeader(), 500);
+  }
   if (event.action == 'Contact Form Shown') {
     WebViewChannel.postMessage('FormShown');
   }
@@ -16,13 +18,58 @@ zE("webWidget:on", "userEvent", function (event) {
   
 });
 
-// //messenger
-// zE("messenger:on", "userEvent", function (event) {
-//   if (event.action == "open") {
-//     WebViewChannel.postMessage('openWebView');
-//   }
-//   if (event.action == "close") {
-//     WebViewChannel.postMessage('closeWebView');
-//   }
-// });
-// zE("messenger:set", "cookies", true)
+
+function hideHeader() {
+  frameElement = document.getElementById("webWidget");
+  if (frameElement) {
+    doc = frameElement.contentDocument;
+
+    doc.querySelector("footer .cYBQJZ").placeholder = "escreva sua mensagem...";
+
+    var tag = document.createElement("style");
+
+    var css = document.createTextNode(
+      `.kSCaYV {
+        background-color: white !important;
+        padding: 24px !important;
+        padding-bottom: 24px !important;
+      }
+      .kSCaYV > div > div {
+        margin-top: -8px !important;
+      }
+      textarea[data-garden-id='forms.textarea'] {
+        height: 56px !important;
+        width: 100% !important;
+        background-color: #F4F3F5 !important;
+        border-radius: 0 !important;
+        display: flex !important;
+        align-items: center !important;
+      }
+      textarea[name='description'].bcdGHE {
+        width: unset !important;
+        height: unset !important;
+        min-width: unset !important;
+
+      }
+      button[data-garden-id='buttons.icon_button'].fgvvBn {
+        height: 56px !important;
+        background-color: #F4F3F5 !important;
+        border-radius: 0 !important;
+        display: flex !important;
+        justify-content: center !important;
+        align-items: center !important;
+        margin-left: -1px !important;
+      }
+      button[data-garden-id='buttons.icon_button'].fgvvBn * {
+        fill: #3F3FF9 !important;
+      }
+      .fgvvBn .sc-bwzfXH {
+        width: unset !important;
+        height: unset !important;
+        min-width: unset !important;
+      }`
+    );
+    tag.appendChild(css);
+    doc.head.appendChild(tag);
+  }
+}
